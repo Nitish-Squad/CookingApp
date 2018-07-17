aeba3eb4e190576fbbc74af4a8e702fe19521f0b
Muhammad Younis <myounis@fb.com>
2018-07-17 15:07:37 -0700

    Added function to populate timeline

--- app/src/main/java/me/ninabernick/cookingapplication/MainActivity.java
+++ app/src/main/java/me/ninabernick/cookingapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvTest;
     TextView tvTest2;
     TextView tvTest3;
-    private ArrayList<Recipe> recipes;
+    public ArrayList<Recipe> recipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +29,53 @@ public class MainActivity extends AppCompatActivity {
         tvTest2 = (TextView) findViewById(R.id.tvTest2);
         tvTest3 = (TextView) findViewById(R.id.tvTest3);
 
+        // initialize the recipes list
+        recipes = new ArrayList<>();
+
+        getRecipes();
+
+
+        String string = "test";
+
+
+
+        // code used to add steps to the sample recipe on the Parse Server
+
+        /*try {
+            json.put("text","Put both slices together");
+        } catch (JSONException e1) {
+            e1.printStackTrace();
+        }
+
+        try {
+            json.put("icon","TBD");
+        } catch (JSONException e2) {
+            Log.d("MainActivity","Failed to save icon value");
+            e2.printStackTrace();
+        }
+
+        try {
+            json.put("time","One minute");
+        } catch (JSONException e3) {
+            Log.d("MainActivity","Failed to save time value");
+            e3.printStackTrace();
+        }
+
+        String step = json.toString();
+
+        tvTest.setText(step);
+
+        recipe.add("steps",step);
+        recipe.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                // just do nothing
+            }
+        });*/
+
+    }
+
+    public void getRecipes(){
         final Recipe.Query recipeQuery = new Recipe.Query();
 
         // place to order the recipes in some manner (ex: rating, when created, etc)
@@ -40,58 +87,22 @@ public class MainActivity extends AppCompatActivity {
             public void done(List<Recipe> objects, ParseException e) {
                 if (e == null)
                 {
-                    Recipe recipe = objects.get(0);
+                    for (int i = 0; i < objects.size(); i ++){
+                        Recipe recipe = objects.get(i);
+                        recipes.add(recipe);
 
-
-                    tvTest.setText(recipe.getStepText(0));
-                    tvTest2.setText(recipe.getStepImageURL(0));
-                    tvTest3.setText(recipe.getStepTime(0));
-
-                    Integer test_int = recipe.getNumberofSteps();
-
-
-                    // code used to add steps to the sample recipe on the Parse Server
-
-                    /*try {
-                        json.put("text","Put both slices together");
-                    } catch (JSONException e1) {
-                        e1.printStackTrace();
+                        // code to update the adapter for the recycler view
+                        // recipeAdapter.notifyItemInserted(recipes.size() - 1);
                     }
-
-                    try {
-                        json.put("icon","TBD");
-                    } catch (JSONException e2) {
-                        Log.d("MainActivity","Failed to save icon value");
-                        e2.printStackTrace();
-                    }
-
-                    try {
-                        json.put("time","One minute");
-                    } catch (JSONException e3) {
-                        Log.d("MainActivity","Failed to save time value");
-                        e3.printStackTrace();
-                    }
-
-                    String step = json.toString();
-
-                    tvTest.setText(step);
-
-                    recipe.add("steps",step);
-                    recipe.saveInBackground(new SaveCallback() {
-                        @Override
-                        public void done(ParseException e) {
-                            // just do nothing
-                        }
-                    });*/
                 }
                 else{
                     Log.d("MainActivity","Failed to get all the recipes.");
                     e.printStackTrace();
                 }
+
+                String breakpoint = "only here so I can set a breakpoint";
             }
         });
-
-
 
 
     }
