1adba218a5485f133d6e7139b60cb32eea14084e
Nina Bernick <ninabernick@fb.com>
2018-07-24 17:25:17 -0700

    adding comments

--- app/src/main/java/me/ninabernick/cookingapplication/RecipeDetailsActivity.java
+++ app/src/main/java/me/ninabernick/cookingapplication/RecipeDetailsActivity.java
@@ -1,7 +1,9 @@
 package me.ninabernick.cookingapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -28,6 +30,42 @@ public class RecipeDetailsActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        ViewPager.OnPageChangeListener pagerListener = new ViewPager.OnPageChangeListener() {
+            boolean lastPageChange = false;
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels){
+                int lastIdx = fAdapter.getCount() - 1;
+
+                Log.d("step position", "pos:" + position);
+                if(lastPageChange && position == lastIdx) {
+                    Intent i = new Intent(RecipeDetailsActivity.this, FinishedRecipeActivity.class);
+                    i.putExtra("recipe", recipe);
+                    startActivity(i);
+                }
+
+            }
+            @Override
+            public void onPageSelected( int i) {
+                //viewPager.setCurrentItem(i/2);
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                int lastIdx = fAdapter.getCount() - 1;
+
+                int curItem = viewPager.getCurrentItem();
+                if(curItem==lastIdx  && state==1){
+                    lastPageChange = true;
+
+                    finish();
+
+                }else  {
+                    lastPageChange = false;
+                }
+            }
+        };
+        viewPager.setOnPageChangeListener(pagerListener);
 
     }
 
